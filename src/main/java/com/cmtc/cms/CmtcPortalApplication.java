122fe092419004abf0cd401570d7a301fc61a05b
Rajkumar saad <rohitsaad01@gmail.com>
2025-11-12 14:58:58 +0530

    Initial commit - CMTC Portal backend (Spring Boot)

---
+++ src/main/java/com/cmtc/cms/CmtcPortalApplication.java
@@ -0,0 +1,13 @@
+package com.cmtc.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmtcPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmtcPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmtc/cms/CmtcPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmtc.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmtcPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
